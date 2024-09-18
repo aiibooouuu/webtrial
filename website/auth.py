d8aecea8c2fa2448b29f568de49f857cf8f557f2
PippyUrkel <bruhbruhbruh238@gmail.com>
2024-09-18 21:45:39 +0530

    temporary commit with errors

--- website/auth.py
+++ website/auth.py
@@ -1,10 +1,14 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 auth = Blueprint('auth', __name__)
 
-@auth.route('/login')
+@auth.route('/login-student')
 def login():
-    return "<p>Logged in</p>"
+    return render_template("l1.html")
+
+@auth.route('/login-instructor')
+def login():
+    return render_template('l2.html')
 
 @auth.route('/logout')
 def logout():
