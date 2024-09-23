7c4e156f0e23a1edf01ae7d79440d1da19de5e0a
PippyUrkel <bruhbruhbruh238@gmail.com>
2024-09-23 21:43:36 +0530

    added base template and linked files accordingly

--- website/views.py
+++ website/views.py
@@ -4,20 +4,20 @@ views = Blueprint('views', __name__)
 
 @views.route('/')
 def home():
-    return render_template("index.html")
+    return render_template("home.html")
 
 @views.route('/student-dashboard')
 def std_dashboard():
-    return render_template('stddashboard.html')
+    return render_template('student-dashboard.html', title="Welcome Student")
 
 @views.route('/instructor-dashboard')
 def inst_dashboard():
-    return render_template('instdashboard.html')
+    return render_template('instructor-dashboard.html', title="Welcome Instructor")
 
 @views.route('/student-courses')
 def student_courses():
-    return render_template('avlblcourses.html')
+    return render_template('student-courses.html', title="Available Courses")
 
 @views.route('/student-mentors')
 def student_mentors():
-    return render_template('mentor.html')
+    return render_template('student-mentors.html', title="Student Mentors")
