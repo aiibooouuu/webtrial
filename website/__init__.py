064b8e451d7b1332df7e13d78c1b814ab7c438f3
PippyUrkel <bruhbruhbruh238@gmail.com>
2024-09-29 16:20:11 +0530

    changed some things in init and added models.py

--- website/__init__.py
+++ website/__init__.py
@@ -1,9 +1,13 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 from website.forms import RegistrationForm, LoginForm
 
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'WELCOMEGAGA@238'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+    db = SQLAlchemy(app)
+
     from .views import views
     from .auth import auth
 
